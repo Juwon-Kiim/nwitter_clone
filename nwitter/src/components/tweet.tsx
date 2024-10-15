bdaaada4ea5661928a05347a5f3f2b1573035f22
김주원 <sa6812000@naver.com>
2024-10-15 22:15:55 +0900

    update tweet

--- nwitter/src/components/tweet.tsx
+++ nwitter/src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { ITweet } from './timeline';
 import { auth, db, storage } from '../firebase';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { deleteObject, ref } from 'firebase/storage';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
     display: grid;
@@ -25,6 +26,18 @@ const Payload = styled.p`
     margin: 10px 0px;
     font-size: 18px;
 `;
+const EditButton = styled.button`
+    background-color: #00db00;
+    margin-right: 5px;
+    color: white;
+    font-weight: 600;
+    border: 0;
+    font-size: 12px;
+    padding: 5px 10px;
+    text-transform: uppercase;
+    border-radius: 5px;
+    cursor: pointer;
+`;
 const DeleteButton = styled.button`
     background-color: tomato;
     color: white;
@@ -36,9 +49,57 @@ const DeleteButton = styled.button`
     border-radius: 5px;
     cursor: pointer;
 `;
+const OKButton = styled.button`
+    background-color: #00db00;
+    margin-right: 5px;
+    color: white;
+    font-weight: 600;
+    border: 0;
+    font-size: 12px;
+    padding: 5px 10px;
+    text-transform: uppercase;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+const CancelButton = styled.button`
+    background-color: tomato;
+    color: white;
+    font-weight: 600;
+    border: 0;
+    font-size: 12px;
+    padding: 5px 10px;
+    text-transform: uppercase;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+const Edit = styled.textarea`
+    border: 2px solid white;
+    padding: 20px;
+    border-radius: 20px;
+    font-size: 16px;
+    color: white;
+    background-color: black;
+    width: 100%;
+    resize: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+        Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+        sans-serif;
+    &::placeholder {
+        font-size: 16px;
+    }
+    &:focus {
+        outline: none;
+        border-color: #1d9bf0;
+    }
+`;
 
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+    const [isEditing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweet);
     const user = auth.currentUser;
+    const onEdit = () => {
+        setEditing(true);
+    }
     const onDelete = async () => {
         const ok = confirm("Are you sure you want to delete this tweet?");
         if (!ok ||user?.uid !== userId) return;
@@ -54,17 +115,34 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
             
         }
     }
+    const onOK = async () => {
+        if (user?.uid !== userId || tweet.length > 180) return;
+        try {
+            await updateDoc(doc(db, "tweets", id), {
+                tweet: newTweet,
+            });
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setEditing(false);
+        }
+    };
+    const onCancel = () => {
+        setEditing(false);
+    };
+    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewTweet(e.target.value);
+    }
     return (
         <Wrapper>
             <Column>
                 <Username>{username}</Username>
-                <Payload>{tweet}</Payload>
-                {user?.uid===userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
+                {isEditing ? <Edit onChange={onChange}>{newTweet}</Edit> : <Payload>{tweet}</Payload>}
+                {user?.uid === userId ? isEditing ? <OKButton onClick={onOK}>OK</OKButton> : <EditButton onClick={onEdit}>Edit</EditButton> : null}
+                {user?.uid === userId ? isEditing ? <CancelButton onClick={onCancel}>Cancel</CancelButton> : <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
             </Column>
             <Column>
-                {photo ? (
-                    <Photo src={photo} />
-                ) : null}
+                {photo ? <Photo src={photo} /> : null}
             </Column>
         </Wrapper>
     )
